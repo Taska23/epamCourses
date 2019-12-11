b8e37a5d8df0c72a0356f9946c4895cb891449f3
Taska23 <77rex7@gmail.com>
2019-12-11 16:24:53 +0200

    Added Task 2.2 solution

---
+++ src/com/epam/courses/homework/task2/task2_2/model/BookBuilder.java
@@ -0,0 +1,43 @@
+package com.epam.courses.homework.task2.task2_2.model;
+public class BookBuilder {
+    private String Title;
+    private String author;
+    private String publisher;
+    private int publishYear;
+    private int pagesNumber;
+
+    public BookBuilder withName(String name) {
+        this.Title = name;
+        return this;
+    }
+
+    public BookBuilder withAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+
+    public BookBuilder withPublisher(String publisher) {
+        this.publisher = publisher;
+        return this;
+    }
+
+    public BookBuilder withPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+        return this;
+    }
+
+    public BookBuilder withPagesNumber(int pagesNumber) {
+        this.pagesNumber = pagesNumber;
+        return this;
+    }
+
+    public Book build() {
+        Book book = new Book();
+        book.setTitle(Title);
+        book.setAuthor(author);
+        book.setPublisher(publisher);
+        book.setPublishYear(publishYear);
+        book.setPagesNumber(pagesNumber);
+        return book;
+    }
+}
---
+++ src/com/epam/courses/test.java
@@ -0,0 +1,9 @@
+package com.epam.courses;
+
+public class test {
+    public static void main(String[] args) {
+
+
+    }
+    
+}
